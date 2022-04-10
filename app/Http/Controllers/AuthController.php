89a4886dae198cd6f4a3a5aaeea4067f44293241
asifthen00b <ahnafshahriar92@gmail.com>
2022-04-11 05:26:31 +0600

    added stuff

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 //use MongoDB\Driver\Exception\AuthenticationException;
 
+use Illuminate\Support\Facades\Validator;
+
 class AuthController extends Controller
 {
     /**
@@ -17,16 +19,28 @@ class AuthController extends Controller
         if(!Auth::attempt($req->only('email','password'))){
             throw new AuthenticationException();
         }
-//        return $req->email;
     }
     public function register(Request $req){
+
+        $validation = Validator::make($req->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|max:255',
+            'phone' => 'required|unique:users|max:64'
+        ]);
+        if($validation->fails()){
+            return response()->json(['errors' => $validation->errors(), 'error' => true]);
+        }
+
         $user = new User;
+
         $user -> name = $req->name;
         $user -> email = $req->email;
         $user -> phone = $req->phone;
         $user -> password = bcrypt($req->password);
 
         $user->save();
+
+        return $user;
     }
     public function logout(Request $req){
         Auth::logout();
